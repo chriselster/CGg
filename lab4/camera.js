350423f802e25dea3f5bb5dfe786ed2f7447a7de
Christian Elster <christian.elster0@gmail.com>
2018-05-28 18:18:54 -0300

    Add files via upload

--- lab4/camera.js
+++ lab4/camera.js
@@ -2,26 +2,27 @@ var scene 		= null;
 var scene2;
 var renderer	= null;
 var camera 		= null;
+var camera2;
 var angleX		= 0.007;
 var angleY		= 0.0;
 var angleZ		= 0.0;
-var mesh;
+var mesh,espelho;
 var box,dot,plano;
-var track;
+var track,group;
 var minimapa;
 var clock,color;
 
 function init() {
 
-
+//cena
 	scene = new THREE.Scene();
 	scene2 = new THREE.Scene();
 	color = new THREE.Color('skyblue' );
-	scene.fog = new THREE.Fog(color, 0.1,300);
+	scene.fog = new THREE.Fog(color, 0.1,200);
 	//scene.background = color;
 	//lights
 
-	//camera
+//camera
 	aspectRatio = window.innerWidth/window.innerHeight;
 	minimapa = new THREE.OrthographicCamera(window.innerHeight / - 2, window.innerHeight / 2, window.innerWidth / 2, window.innerWidth / - 2, 1, 1000);
 	minimapa.up = new THREE.Vector3(0,0,-1); 
@@ -29,19 +30,19 @@ function init() {
 	minimapa.position.y = 300;
 	scene2.add(minimapa);
 	camera = new THREE.PerspectiveCamera( 60.0, aspectRatio, 0.1, 300 );
-	camera.position.y = 2;
-	camera.position.z = 20;	
-	camera2 = new THREE.PerspectiveCamera( 60.0, aspectRatio, 0.1, 300 );
+	camera.position.y = 2;	
+	camera2 = new THREE.PerspectiveCamera( 60.0, 2*aspectRatio, 0.1, 300 );
+
 
 	scene.add( camera );
 	scene.add(camera2);
 	
-	//luzes
+//luzes
 	light = new THREE.DirectionalLight(0xefc07f, 1.4);
     light.position.set(300, 400, 50);
     light.position.multiplyScalar(1.3);
-    light.shadow.mapSize.width = 2000;  // default
-	light.shadow.mapSize.height = 2000;
+    light.shadow.mapSize.width = 8000;  // default
+	light.shadow.mapSize.height = 8000;
     light.castShadow = true;
     var d = 300;
     light.shadow.camera.left = -d;
@@ -49,9 +50,7 @@ function init() {
     light.shadow.camera.top = d;
     light.shadow.camera.bottom = -d;
     light.shadow.camera.far =1000;
-
     scene.add(light);
-
     light2 = new THREE.DirectionalLight(0xdfebff, 1.2);
     light2.position.set(300, 400, 50);
     light2.position.multiplyScalar(1.3);
@@ -65,31 +64,43 @@ function init() {
     light2.shadow.camera.bottom = -d;
     light2.shadow.camera.far =1000;
     scene2.add(light2);
-
 	var ambient = new THREE.AmbientLight(0xdfebff,0.2);
 	scene.add( ambient );
 
 
-	//obj
+//Objetos
 	var loader = new THREE.OBJLoader();
 	loader.load('../Assets/Models/city.obj', loadMesh);
 	loader.load('../Assets/Models/city.obj', loadMesh2);
 
+	group = new THREE.Group();
+	group.add(camera);
+	group.position.z = 20;
 	var geometry = new THREE.CircleGeometry( 10, 32 );
 	var material = new THREE.MeshBasicMaterial( { color: 0xff0000 } );
 	dot = new THREE.Mesh( geometry, material );
 	dot.rotation.x = 4.7;
+	dot.position.y = 1;
 	scene2.add( dot );
 
-	geometry = new THREE.SphereGeometry( 20, 60,60 );
-	material = new THREE.MeshBasicMaterial( { color: color } );
-	plano = new THREE.Mesh( geometry, material );
-	plano.side = THREE.DoubleSide;
-	scene.add( plano );
+	var geometry = new THREE.PlaneBufferGeometry( 0.8, 0.14);
+	espelho = new THREE.Reflector( geometry, {
+		clipBias: 0.00003,
+		textureWidth: 1500,
+		textureHeight: 1500,
+		recursion: 1
+	} );
+	espelho.position.x = camera.position.x;
+	espelho.position.y = 2.25;
+	espelho.position.z = camera.position.z-0.6;
+	espelho.rotation.x = 0.18;
+
+	group.add(espelho);
+	scene.add( group );
 
 
 
-	//renderer
+//renderer
 	var w = window.innerWidth, h = window.innerHeight;
 	renderer = new THREE.WebGLRenderer();
 	renderer.setSize(w, h);
@@ -98,14 +109,12 @@ function init() {
 	renderer.shadowCameraNear = 3;
 	renderer.shadowCameraFar = camera.far;
 	renderer.shadowCameraFov = 60;
-
-
 	document.getElementById("WebGL-output").appendChild(renderer.domElement);
 
 
-
+//Controles
 	track
-	controls = new THREE.FirstPersonControls(camera);
+	controls = new THREE.FirstPersonControls(group);
 	controls.lookSpeed = 0.2;
 	controls.movementSpeed = 10;
  	// controls.noFly = true;
@@ -135,26 +144,24 @@ function render() {
 	renderer.clear( true, false, false );
 	renderer.setViewport( 0, 0, w, h );
 	renderer.render( scene, camera );
-	renderer.setViewport( w/2-w/4, h/30, w/2, h/5);
-	dot.position.x = camera.position.x;
-	dot.position.y = camera.position.y;
-	dot.position.z = camera.position.z;
-
-	plano.position.x = camera.position.x;
-	plano.position.y = camera.position.y;
-	plano.position.z = camera.position.z;
 
 
+	// renderer.setViewport( w/2-w/4, h/30, w/2, h/5);
 
-	camera2.position.x = camera.position.x;
-	camera2.position.y = camera.position.y;
-	camera2.position.z = camera.position.z;
-	camera2.rotation.y = -camera.rotation.y;
+	
+	// camera2.position.x = camera.position.x;
+	// camera2.position.y = camera.position.y;
+	// camera2.position.z = camera.position.z;
+	// camera2.rotation.x = camera.rotation.x;
+	// camera2.rotation.y = camera.rotation.y+Math.PI;
+	// camera2.rotation.z = camera.rotation.z;
+
+	// renderer.clear( false, true, false );
+	// renderer.render( scene, camera2 );
+
 	renderer.clear( false, true, false );
-	renderer.render( scene, camera2 );
-
-	renderer.clear( false, true, false ); // clear the depth buffer -- thanks @WestLangley!
-	// minimap (overhead orthogonal camera)
+	dot.position.x = group.position.x;
+	dot.position.z = group.position.z;
 	renderer.setViewport( 0, -h/8, w/6, w/4);
 	renderer.render( scene2, minimapa );
 	requestAnimationFrame( render );
